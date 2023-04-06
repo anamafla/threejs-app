7c7452b9d0181bb0e27e1b2488cd555767ad1e8f
Ana Mafla <anamafla@hotmail.com>
2023-04-06 11:18:55 -0500

    ThreeJS Lighting

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -18,6 +18,31 @@ import {
 
 const Customizer = () => {
   const snap = useSnapshot(state);
+
+  const [file, setFile] = useState("");
+  const [prompt, setPrompt] = useState("");
+  const [generatingImg, setGeneratingImg] = useState(false);
+
+  const [activeEditorTab, setActiveEditorTab] = useState("");
+  const [activeFilterTab, setActiveFilterTab] = useState({
+    logoShirt: true,
+    stylishShirt: false
+  });
+
+  // show tab content depending on the activeTab
+  const generateTabContent = () => {
+    switch (activeEditorTab) {
+      case "colorpicker":
+        return <ColorPicker />;
+      case "filepicker":
+        return <FilePicker />;
+      case "aipicker":
+        return <AIPicker />;
+      default:
+        return null;
+    }
+  };
+
   return (
     <AnimatePresence>
       {!snap.intro && (
@@ -30,8 +55,15 @@ const Customizer = () => {
             <div className="flex items-center min-h-screen">
               <div className="editortabs-container tabs">
                 {EditorTabs.map(tab => (
-                  <Tab key={tab.name} tab={tab} handleClick={() => {}}></Tab>
+                  <Tab
+                    key={tab.name}
+                    tab={tab}
+                    handleClick={() => {
+                      setActiveEditorTab(tab.name);
+                    }}
+                  ></Tab>
                 ))}
+                {generateTabContent()}
               </div>
             </div>
           </motion.div>
